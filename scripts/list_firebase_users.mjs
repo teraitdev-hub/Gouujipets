a0a86417cabc5fe06ee3d980859e2af4dfd87e9e
terait.dev <terait.dev@gmail.com>
2026-07-28 18:25:56 +0530

    chore: clean up unwanted scratch files and temp files

--- scripts/list_firebase_users.mjs
+++
@@ -1,8 +0,0 @@
-import admin from 'firebase-admin';
-
-// Initialize the Firebase Admin SDK
-// Wait, we need a service account key to use firebase-admin.
-// But we don't have a service account key downloaded in this workspace.
-
-// Is there a service account key in the workspace?
-// Let's check for any JSON files that look like service accounts.
